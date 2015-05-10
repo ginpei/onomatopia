d31ac573cf6871c57fb0abc8b9332b29f0c40c5c
ginpei <ginpei@gmail.com>
2015-05-10 14:41:38 -0700

    show top onomatopes in home page

--- app/controllers/miscellaneous_controller.rb
+++ app/controllers/miscellaneous_controller.rb
@@ -1,5 +1,20 @@
 class MiscellaneousController < ApplicationController
   def home
+    @top_onomatopes = []
+    @top_onomatopes.push(get_top_onomatopes_list_item(1, 'KABOOM'))
+    @top_onomatopes.push(get_top_onomatopes_list_item(2, 'THUMP'))
+    @top_onomatopes.push(get_top_onomatopes_list_item(2, 'CHEER'))
+    @top_onomatopes.push(get_top_onomatopes_list_item(3, 'SOB'))
+    @top_onomatopes.push(get_top_onomatopes_list_item(3, 'CRASH'))
+    @top_onomatopes.push(get_top_onomatopes_list_item(3, 'WOOF'))
+
     @onomatopes = Onomatope.all
   end
+
+  private
+    def get_top_onomatopes_list_item(span, name)
+      onomatope = Onomatope.find_by_name(name)
+      illustration = onomatope.illustrations.first
+      { span: span, onomatope: onomatope, illustration: illustration }
+    end
 end
