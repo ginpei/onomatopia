dc22be4f44a0b2dea304dde8679ef69b08e100ad
ginpei <ginpei@gmail.com>
2015-05-10 12:06:43 -0700

    make sure onomatope's name upper case

--- app/models/onomatope.rb
+++ app/models/onomatope.rb
@@ -2,6 +2,10 @@ class Onomatope < ActiveRecord::Base
   validates :name, :presence => true
   has_many :illustrations
 
+  def name=(s)
+    write_attribute(:name, s.upcase)
+  end
+
   def to_param
     name
   end
