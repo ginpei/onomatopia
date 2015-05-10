aa091877988e26ff9d2e84bbc8753531463b901a
ginpei <ginpei@gmail.com>
2015-05-09 22:42:25 -0700

    enable S3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    region: Rails.application.secrets.awd_region
+  }
+
+  config.fog_directory = Rails.application.secrets.awd_directory
+end
