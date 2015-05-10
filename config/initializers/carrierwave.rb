5114a1d1d5fa0c1f1082bd219cfd7a9c4d2f58bc
ginpei <ginpei@gmail.com>
2015-05-09 22:49:57 -0700

    use local disk for development

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,15 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: Rails.application.secrets.awd_region
-  }
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: Rails.application.secrets.awd_region
+    }
+  else
+    config.storage = :file
+  end
 
   config.fog_directory = Rails.application.secrets.awd_directory
 end
