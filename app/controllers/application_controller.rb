6a9abd17c1f1c6313c22b8dfbe6be45b9f3a835c
ginpei <ginpei@gmail.com>
2015-05-10 21:07:23 -0700

    markdown helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :md
+
+  def md(html)
+    unless @md_renderer
+      options = {
+        autolink: true,
+        tables: true
+      }
+      @md_renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+    end
+    @md_renderer.render(html).html_safe
+  end
 end
