2b1a38784e559ca568d4807e2ab5ee0cc8d076f5
ginpei <ginpei@gmail.com>
2015-05-18 20:09:14 -0700

    fix an error when reset DB

--- db/migrate/20150510181113_forbid_onomatope_nil_for_illustrations.rb
+++ db/migrate/20150510181113_forbid_onomatope_nil_for_illustrations.rb
@@ -1,9 +1,11 @@
 class ForbidOnomatopeNilForIllustrations < ActiveRecord::Migration
   def up
-    Onomatope.create( name: 'KABOOM' ) if Onomatope.count < 1
-    onomatope = Onomatope.first
-    Illustration.all.each do |illustration|
-      illustration.update( onomatope: onomatope )
+    if Illustration.count > 0
+      Onomatope.create( name: 'KABOOM' ) if Onomatope.count < 1
+      onomatope = Onomatope.first
+      Illustration.all.each do |illustration|
+        illustration.update( onomatope: onomatope )
+      end
     end
     change_column :illustrations, :onomatope_id, :integer, null: false
   end
