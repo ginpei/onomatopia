6aa2415481ddd09ec2b0ac31596de9f874857844
ginpei <ginpei@gmail.com>
2015-05-21 18:02:55 -0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def create_onomatope(data)
+  onomatope = Onomatope.create
+
+  synonym = onomatope.synonyms.create(
+    name: data[:name])
+  onomatope.update(top_synonym_id: synonym.id)
+
+  illustration = onomatope.illustrations.create(
+    image: seed_image(data[:name]))
+
+  explanation = onomatope.explanations.create(
+    outline: data[:outline],
+    description: data[:description])
+end
+
+def seed_image(name)
+  File.open(File.join(Rails.root, "/db/seed_images/#{name}.png"))
+end
+
+create_onomatope(
+  name: 'KABOOM',
+  outline: 'Explosion.',
+  description: 'Explosion. Explosion. **Explosion**. Explosion. Explosion.'
+)
+create_onomatope(
+  name: 'THUMP',
+  outline: 'Sitting down.',
+  description: 'What!? From a standing position! Such jumping power in his knees alone!'
+)
+create_onomatope(
+  name: 'CHEER',
+  outline: 'Happy.',
+  description: 'Ha-ha-ha.'
+)
+create_onomatope(
+  name: 'SOB',
+  outline: 'Sad.',
+  description: 'Sick sick...'
+)
+create_onomatope(
+  name: 'CRASH',
+  outline: 'Something is broken.',
+  description: 'Broken heart.'
+)
+create_onomatope(
+  name: 'WOOF',
+  outline: 'By doggie.',
+  description: 'Woof! Bow wow! Wang wang!'
+)
