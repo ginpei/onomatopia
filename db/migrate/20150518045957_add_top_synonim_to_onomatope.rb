b44676c69d3abd054523047e7c6aaa272b0c86ed
ginpei <ginpei@gmail.com>
2015-05-25 10:06:42 -0700

    don't use models in migration

--- db/migrate/20150518045957_add_top_synonim_to_onomatope.rb
+++ db/migrate/20150518045957_add_top_synonim_to_onomatope.rb
@@ -1,25 +1,13 @@
 class AddTopSynonimToOnomatope < ActiveRecord::Migration
   def up
-    add_column :onomatopes, :top_synonym_id, :int
-    Onomatope.all.each do |onomatope|
-      top = Synonym.find_by_name(onomatope.name)
-      if top.nil?
-        top = Synonym.create(onomatope: onomatope, name: onomatope.name)
-      end
-      onomatope.update(top_synonym_id: top.id)
-    end
-    change_column :onomatopes, :top_synonym_id, :int, null: false
+    add_column :onomatopes, :top_synonym_id, :int, default: 1
+    change_column :onomatopes, :top_synonym_id, :int, default: nil, null: false
     remove_column :onomatopes, :name
   end
 
   def down
-    add_column :onomatopes, :name, :string
-    Onomatope.all.each do |onomatope|
-      top = Synonym.find(onomatope.top_synonym_id)
-      onomatope.update(name: top.name)
-      top.destroy
-    end
-    change_column :onomatopes, :name, :string, null: false
+    add_column :onomatopes, :name, :string, default: 'KABOOM'
+    change_column :onomatopes, :name, :string, default: nil, null: false
     remove_column :onomatopes, :top_synonym_id
   end
 end
